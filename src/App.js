0d5f2d00e0a38b7336ae4c87fa3451d80d7265a5
NaveenJayaram94 <naveenjayaram04@gmail.com>
2019-09-25 19:12:27 +0530

    multiple reducer with multiple state access

--- src/App.js
+++ src/App.js
@@ -6,22 +6,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>Age : <span>{this.props.age}</span></div>
-        <button onClick={this.props.onAgeUp}>Age Up</button>
-        <button onClick={this.props.onAgeDown}>Age Down</button>
+        <div className="col">
+          <div><span>A:</span><span>{this.props.a}</span></div>
+          <button onClick={() => this.props.onUpdateA(this.props.b)}>Update A</button>
+        </div>
+        <div className="col">
+          <div><span>B:</span><span>{this.props.b}</span></div>
+          <button onClick={() => this.props.onUpdateB(this.props.a)}>Update B</button>
+        </div>
       </div>
     );
   }
 }
 const mapStateToProps = (state) => {
   return {
-    age: state.age
+    a: state.rA.a,
+    b: state.rB.b
   }
 }
 const mapDispatchToProps = (dispatch) => {
   return {
-    onAgeUp: () => dispatch({ type: "AGE_UP" }),
-    onAgeDown: () => dispatch({ type: "AGE_DOWN" }),
+    onUpdateA: (b) => dispatch({ type: "UPDATE_A", b: b }),
+    onUpdateB: (a) => dispatch({ type: "UPDATE_B", a: a }),
   }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/store/reducerB.js
+++ src/store/reducerB.js
@@ -1,18 +1,11 @@
 const initialState = {
-  a: 1,
   b: 1,
 }
 const reducer = (state = initialState, action) => {
-  if (action.type == "UPDATE_A") {
-    return {
-      ...state,
-      a: state.a + state.b
-    }
-  }
   if (action.type == "UPDATE_B") {
     return {
       ...state,
-      b: state.a + state.b
+      b: action.a + state.b
     }
   }
   return state;
